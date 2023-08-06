1f78b3237227f1714799a34b609d84c027d0337f
blakepk <140014430+blakepk@users.noreply.github.com>
2023-08-06 09:58:55 -0500

    paso 1

---
+++ abrir_pagina.py
@@ -0,0 +1,5 @@
+# # Para abrir la pagina, en YT los videos son antiguos, ahora solo necesito esta ruta:
+
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get("https://www.google.com/") 
